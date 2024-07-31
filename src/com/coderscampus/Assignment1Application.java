18e0aa0369de74fc2604c5ea8c861906659bce99
Yehoshua Holstein <holsteinyehoshua@gmail.com>
2024-07-31 11:23:32 -0400

    V2 changes

--- src/com/coderscampus/Assignment1Application.java
+++ src/com/coderscampus/Assignment1Application.java
@@ -3,7 +3,12 @@ package com.coderscampus;
 public class Assignment1Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// generate a random number
+		
+		// and collect user input
+		
+		// etc.
 
 	}
 
